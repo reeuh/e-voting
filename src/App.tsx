72216efcebdd95e88e7b163fb24e6f20282c4fac
Faye Lacsi <113992925+reeuh@users.noreply.github.com>
2025-03-20 20:23:57 +0800

    updated admindashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
-import {Route, Routes, BrowserRouter as Router, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login'
 import Register from './pages/Register';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} /> 
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
   )
 }
 
